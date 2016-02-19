8a37f1dd25cf2f8667d9f6cadc3bd7d77d52ed6a
Pascal Deschenes <pdeschen@nuecho.com>
2016-02-19 14:33:29 -0500

    fix git output silent

--- hot-deploy.sh
+++ hot-deploy.sh
@@ -2,9 +2,9 @@
 EXTENSION=/etc/asterisk/enabled/extensions
 cd ${EXTENSION};
 if [ -d demo ]; then
-  cd demo;git pull > /dev/null;cd - > /dev/null;
+  cd demo;git pull 2>&1 /dev/null;cd - > /dev/null;
 else
-  git clone https://github.com/pdeschen/demo.git > /dev/null;
+  git clone https://github.com/pdeschen/demo.git 2>&1 /dev/null;
 fi
 cd - > /dev/null;
 echo "#include ${EXTENSION}/demo/demo.conf"
